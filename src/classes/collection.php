d3e9a1fc4d66030d9c94e8a75667bf8a566a9fe8
Steven Jackson <stevenpjackson0@gmail.com>
2020-01-25 12:27:44 -0600

    Add (collection.php>Hunk 1) : Create constructor that implements repository interface

--- src/classes/collection.php
+++ src/classes/collection.php
@@ -1,7 +1,28 @@
 <?php
+/**
+ * Main controller applicaiton controller.
+ * Implements repository on construct. 
+ */
 
-class Collection
+class Collection implements Iterator, Countable 
 {
+    protected $repo;
+    public $collection;
 
+/**
+ * Construct collection object.
+ * @param Interface implement 
+ * @param Int
+ * @param Int   
+ */
+    public function construct(RepositoryInterface $repo, $id = null,
+    $field = 'id'){
+        $this->repo = $repo;
+        if(!empty($id)){
+            $this->collection = $this->repo->find('posts', $id, $field);
+        }else{
+            $this->collection = $this->repo->all('posts');
+        }
 
+    }
 }
