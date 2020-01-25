85a89125dc5b08fdebcf69eb0c06cdbcd53d4b8a
Steven Jackson <stevenpjackson0@gmail.com>
2020-01-25 13:43:42 -0600

    Test 2 (index.php>Hunk 1) : Instantiate Collection object that takes repository on construction and uses foreach to echo titles. Move (collection.php>Hunk 1) : Adjust comment formatting. Add (collection.php>Hunk 2) : Create method calls from Iterator and Countable interface implementations to manipulate repository arrays. Test 2 (collection.php>Hunk 2) :  Add var_dump and magic constant __METHOD__ to member methods in order to view their inner logic.

--- index.php
+++ index.php
@@ -2,12 +2,20 @@
 //Autoload all classes
 require_once "src/config.php";
 
-$content = new Collection();
+//Test 2 new collection object 
+//This will pull all the posts
+$content = new Collection($repo);
+
 $title = "My Website";
 
 require 'views/header.php';
 
 //Test retrieve all posts
-var_dump($repo->all('posts'));
+//var_dump($repo->all('posts'));
+
+//Test 2 Loop through each post and return a title
+foreach($content as $item){
+    echo $item->title;
+}
 
 require 'views/footer.php';
--- src/classes/collection.php
+++ src/classes/collection.php
@@ -4,17 +4,19 @@
  * Implements repository on construct. 
  */
 
+ //Must implment the methods for Iterator and Countable
+ //Errors will be thrown if methods are not implemented 
 class Collection implements Iterator, Countable 
 {
     protected $repo;
     public $collection;
 
-/**
- * Construct collection object.
- * @param Interface implement 
- * @param Int
- * @param Int   
- */
+   /**
+    * Construct collection object.
+    * @param Interface implement 
+    * @param Int
+    * @param Int   
+    */
     public function __construct(RepositoryInterface $repo, $id = null,
     $field = 'id'){
         $this->repo = $repo;
@@ -23,6 +25,70 @@ class Collection implements Iterator, Countable
         }else{
             $this->collection = $this->repo->all('posts');
         }
+    }
 
+   /**
+    * Current is a built in method implemented by
+    * the iterator interface that uses a pointer 
+    * to save the current position
+    * within an array. 
+    */
+    public function current(){
+        //Test 2 use magic constant __METHOD__ 
+        //Returns the current pointer of the collection array 
+        var_dump(__METHOD__);
+        return current($this->collection);
+    }
+    
+   /**
+    * Key is a built in method implemented by
+    * the iterator interface that gives you the key
+    * of the item the pointer is on. 
+    */
+    public function key(){
+        var_dump(__METHOD__);
+        return key($this->collection);
+    }
+
+   /**
+    * Next is a built in method implemented by
+    * the iterator interface that returns the
+    * next value in an array which the internal pointer
+    * is pointing to. 
+    */
+    public function next(){
+        var_dump(__METHOD__);
+        return next($this->collection);
+    }
+
+   /**
+    * Rewind is a built in method implemented by
+    * the iterator interface and called with the reset 
+    * keyword which resets the pointer of the array 
+    * to the first items. 
+    */
+    public function rewind(){
+        var_dump(__METHOD__);
+        return reset($this->collection);
+    }
+  
+   /**
+    * Valid is a built in method implemented by
+    * the iterator interface and called with the key 
+    * keyword which will verify the current key is valid.
+    */
+    public function valid(){
+        var_dump(__METHOD__);
+        return key($this->collection) !== null;
+    }
+
+   /**
+    * Count is a built in method implemented by
+    * the countable interface which returns the
+    * number of items within our array.
+    */
+    public function count(){
+        var_dump(__METHOD__);
+        return count($this->collection);
     }
 }
